d363429efe0821ba05ed94f52d3db071f9ec4df2
Tan Nguyen <tannp27@gmail.com>
2023-06-23 16:58:19 +0700

    fix shellcheck v2

--- run.sh
+++ run.sh
@@ -2,7 +2,8 @@
 
 # Load environment variables from .env file
 set -o allexport
-source "$(dirname "${BASH_SOURCE[0]}")/.env"
+# shellcheck disable=SC1091
+source .env
 set +o allexport
 
 php -S "$APP_HOST":"$APP_PORT"
