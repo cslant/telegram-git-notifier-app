124f1f034450f3ed4878a5fdb0a80a4c5a480e4e
Tan Nguyen <tannp27@gmail.com>
2023-06-28 00:15:40 +0700

    (#33) update template message for some events v3

--- resources/events/ping/default.php
+++ resources/events/ping/default.php
@@ -6,15 +6,15 @@
 $message = "♻️ <b>Connection Successful</b>\n\n";
 
 if (isset($payload->organization)) {
-    $message .= "Organization: <b>{$payload->organization->login}</b>\n";
+    $message .= "🏢 Organization: <b>{$payload->organization->login}</b>\n";
 }
 
 if (isset($payload->repository)) {
-    $message .= "Repository: <b>{$payload->repository->full_name}</b>\n";
+    $message .= "📦 Repository: <b>{$payload->repository->full_name}</b>\n";
 }
 
 if (isset($payload->sender)) {
-    $message .= "Sender (triggered the event): <b>{$payload->sender->login}</b>\n";
+    $message .= "🙋 Sender (triggered the event): <b>{$payload->sender->login}</b>\n";
 }
 
 echo $message;
