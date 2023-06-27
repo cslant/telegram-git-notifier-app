85944dc4d32a99da4c7098bc5888df9c79ece69d
Tan Nguyen <tannp27@gmail.com>
2023-06-27 18:02:22 +0700

    add get template data for event and tool bot

--- src/Helpers/ConfigHelper.php
+++ src/Helpers/ConfigHelper.php
@@ -5,6 +5,7 @@ namespace TelegramGithubNotify\App\Helpers;
 class ConfigHelper
 {
     public array $config;
+    public const VIEW_PATH = 'resources';
 
     public function __construct()
     {
@@ -25,5 +26,30 @@ class ConfigHelper
             $result = $result[$value];
         }
         return $result;
+    }
+
+    /**
+     * Return template data
+     *
+     * @param $partialPath
+     * @param array $data
+     * @return bool|string
+     */
+    public function getEventTemplate($partialPath, array $data = []): bool|string
+    {
+        $viewPathFile = self::VIEW_PATH . '/' . str_replace('.', '/', $partialPath) . '.php';
+
+        if (!file_exists($viewPathFile)) {
+            return '';
+        }
+
+        extract($data);
+
+        ob_start();
+        require $viewPathFile;
+        $content = ob_get_contents();
+        ob_end_clean();
+
+        return $content;
     }
 }
