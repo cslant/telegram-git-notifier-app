17c0fbee6f6dddc9613819c27b5617e2435f4219
Tan Nguyen <tannp27@gmail.com>
2023-07-05 15:51:26 +0700

    update workflow run event

--- resources/events/workflow_run/completed.php
+++ resources/events/workflow_run/completed.php
@@ -7,10 +7,18 @@ if ($payload->workflow_run->conclusion === 'success') {
     $message = "🎉 <b>Workflow Completed</b> from <a href=\"{$payload->repository->html_url}\">{$payload->repository->full_name}</a>\n\n";
 
     $message .= "Done workflow: 🎉 <b>{$payload->workflow_run->name}</b> ✨ \n\n";
-} else {
-    $message = "🚫 <b>Canceled Workflow</b> from <a href=\"{$payload->repository->html_url}\">{$payload->repository->full_name}</a>\n\n";
+} elseif ($payload->workflow_run->conclusion === 'failure') {
+    $message = " <b>Workflow Failed</b> from <a href=\"{$payload->repository->html_url}\">{$payload->repository->full_name}</a>\n\n";
 
-    $message .= "Failed workflow: 🚫 <b>{$payload->workflow_run->name}</b> ❌ \n\n";
+    $message .= "Failed workflow: 🚨 <b>{$payload->workflow_run->name}</b> ❌ \n\n";
+} elseif ($payload->workflow_run->conclusion == 'cancelled') {
+    $message = "🚫 <b>Workflow Cancelled</b> from <a href=\"{$payload->repository->html_url}\">{$payload->repository->full_name}</a>\n\n";
+
+    $message .= "Cancelled workflow: 🚫 <b>{$payload->workflow_run->name}</b> ❌ \n\n";
+} else {
+    $message = "🚨 <b>Workflow Can't Success</b> from <a href=\"{$payload->repository->html_url}\">{$payload->repository->full_name}</a>\n\n";
+
+    $message .= "Can't Success workflow: 🚨 <b>{$payload->workflow_run->name}</b> ❌ \n\n";
 }
 
 // $message .= "📤 Commit: <b>{$payload->workflow_run->head_commit->message}</b>\n\n";
