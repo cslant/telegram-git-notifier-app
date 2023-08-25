e93e93e45bccb4fb448ae7ee6f1effd44e9eefee
Tan Nguyen <tannp27@gmail.com>
2023-08-25 16:49:39 +0700

    (#87) build: create message template for some event of gitlab v2

--- resources/shared/partials/gitlab/_body.php
+++ resources/shared/partials/gitlab/_body.php
@@ -3,8 +3,20 @@
  * @var $payload mixed
  */
 
-if (isset($event) && isset($payload) && !empty($payload->object_attributes->description)) {
-    $body = $payload->object_attributes->description;
+if (isset($event) && isset($payload)) {
+    if (!empty($payload->object_attributes->description)) {
+        $body = $payload->object_attributes->description;
+    } elseif (!empty($payload->object_attributes->content)) {
+        $body = $payload->object_attributes->content;
+    } elseif (!empty($payload->object_attributes->note)) {
+        $body = $payload->object_attributes->note;
+    } elseif (!empty($payload->object_attributes->body)) {
+        $body = $payload->object_attributes->body;
+    } elseif (!empty($payload->description)) {
+        $body = $payload->description;
+    } else {
+        return '';
+    }
     if (strlen($body) > 50) {
         $body = substr($body, 0, 50) . '...';
     }
