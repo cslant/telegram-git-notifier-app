68a25f8e77c0f7ff100b81a240b672c6cffa15c6
Tan Nguyen <tannp27@gmail.com>
2023-08-25 13:52:05 +0700

    (#87) build: create message template for some event of gitlab

--- resources/events/github/pull_request/partials/_reviewers.php
+++ resources/events/github/pull_request/partials/_reviewers.php
@@ -7,7 +7,7 @@ $textReviewers = '';
 if (isset($payload->pull_request->requested_reviewers) && count($payload->pull_request->requested_reviewers) > 0) {
     $reviewers = [];
     foreach ($payload->pull_request->requested_reviewers as $reviewer) {
-        $reviewers[] = $reviewer->login;
+        $reviewers[] = "<b>{$reviewer->login}</b>";
     }
 
     $textReviewers .= "👥 Reviewers: " . implode(', ', $reviewers) . "\n";
