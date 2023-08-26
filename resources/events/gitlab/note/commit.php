f79c3fe98302559662d54fe39daae8a3c78d5f7a
Tan Nguyen <tannp27@gmail.com>
2023-08-26 22:27:22 +0700

    (#87) build: create message template for some event of gitlab v4

--- resources/events/gitlab/note/commit.php
+++ resources/events/gitlab/note/commit.php
@@ -1,3 +1,14 @@
+<?php
+/**
+ * @var $payload mixed
+ */
 
+$message = "💬 <b>New Comment on Commit</b> - 🦊<a href=\"{$payload->object_attributes->url}\">{$payload->project->path_with_namespace}</a> by <b>{$payload->user->name}</b>\n\n";
 
-// comment issue in gitlab
+$message .= "⚙️ <b>{$payload->commit->message}</b> \n\n";
+
+$message .= "🔗 View Comment: <a href=\"{$payload->object_attributes->url}\">{$payload->commit->id}</a> \n\n";
+
+$message .= require __DIR__ . '/../../shared/partials/gitlab/_body.php';
+
+echo $message;
