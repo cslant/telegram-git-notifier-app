cdad1d5484ea8699e471aa57cbdfc49600c6d469
Tan Nguyen <tannp27@gmail.com>
2023-09-23 22:38:07 +0700

    update composer to using core

--- config/tg-notifier.php
+++ config/tg-notifier.php
@@ -2,7 +2,7 @@
 
 return [
     'app' => [
-        'name' => $_ENV['APP_NAME'] ?? 'Telegram Bot Notify',
+        'name' => $_ENV['APP_NAME'] ?? 'Telegram Git Notifier',
         'url' => $_ENV['APP_URL'] ?? 'http://localhost:3000',
         'timezone' => $_ENV['TIMEZONE'] ?? 'Asia/Ho_Chi_Minh',
         'image' => $_ENV['APP_IMAGE'] ?? 'public/images/github.jpeg',
@@ -12,12 +12,10 @@ return [
         'token' => $_ENV['TELEGRAM_BOT_TOKEN'] ?? '',
         'chat_id' => $_ENV['TELEGRAM_BOT_CHAT_ID'] ?? '',
         'notify_chat_ids' => explode(',', $_ENV['TELEGRAM_NOTIFY_CHAT_IDS'] ?? ''),
-
-        'set_webhook_url' => $_ENV['SET_WEBHOOK_URL'] ?? $_ENV['APP_URL'] . '/webhook/set.php',
     ],
 
     'author' => [
         'contact' => $_ENV['AUTHOR_CONTACT'] ?? 'https://t.me/tannp27',
-        'source_code' => $_ENV['AUTHOR_SOURCE_CODE'] ?? 'https://github.com/lbiltech/telegram-bot-github-notify',
+        'source_code' => $_ENV['AUTHOR_SOURCE_CODE'] ?? 'https://github.com/lbiltech/php-telegram-git-notifier',
     ],
 ];
