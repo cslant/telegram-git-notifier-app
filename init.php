4184f53ee066f53018f6f413471d764b1d9a8928
Tan Nguyen <tannp27@gmail.com>
2023-10-09 22:33:00 +0700

    fix set setting and remove redundant code

--- init.php
+++ init.php
@@ -1,10 +1,5 @@
 <?php
 
-use Dotenv\Dotenv;
-
 require __DIR__ . '/vendor/autoload.php';
 
-$dotenv = Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-
-date_default_timezone_set(config('app.timezone'));
+date_default_timezone_set(config('telegram-git-notifier.app.timezone'));
