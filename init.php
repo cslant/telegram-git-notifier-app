cfa1ccbd3cb5484ab7f8e4dd268aff15f182f098
Tan Nguyen <tannp27@gmail.com>
2023-10-10 17:55:38 +0700

    fix to load environment variables

--- init.php
+++ init.php
@@ -1,5 +1,10 @@
 <?php
 
+use Dotenv\Dotenv;
+
 require __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
 date_default_timezone_set(config('telegram-git-notifier.app.timezone'));
