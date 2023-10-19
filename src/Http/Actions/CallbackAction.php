5c4aa2dcb0a9b1cb6afb5cceb7d05b54c1738d1a
Tan Nguyen <tannp27@gmail.com>
2023-10-20 00:10:29 +0700

    fix: update callback action

--- src/Http/Actions/CallbackAction.php
+++ src/Http/Actions/CallbackAction.php
@@ -3,6 +3,8 @@
 namespace LbilTech\TelegramGitNotifierApp\Http\Actions;
 
 use LbilTech\TelegramGitNotifier\Bot;
+use LbilTech\TelegramGitNotifier\Constants\SettingConstant;
+use LbilTech\TelegramGitNotifier\Exceptions\MessageIsEmptyException;
 use LbilTech\TelegramGitNotifierApp\Services\CallbackService;
 
 class CallbackAction
@@ -15,10 +17,35 @@ class CallbackAction
         Bot $bot,
     ) {
         $this->bot = $bot;
-        $this->callbackService = new CallbackService();
+        $this->callbackService = new CallbackService($bot);
     }
 
-    public function __invoke()
+    /**
+     * @return void
+     * @throws MessageIsEmptyException
+     */
+    public function __invoke(): void
     {
+        $callback = $this->bot->telegram->Callback_Data();
+
+        if (str_contains($callback, SettingConstant::SETTING_CUSTOM_EVENTS)) {
+            $this->bot->eventHandle($callback);
+            return;
+        }
+
+        if (str_contains($callback, SettingConstant::SETTING_BACK)) {
+            $this->callbackService->answerBackButton($callback);
+            return;
+        }
+
+        $callback = str_replace(SettingConstant::SETTING_PREFIX, '', $callback);
+
+        if ($this->bot->setting->updateSetting($callback, !$this->bot->setting->getSettings()[$callback])) {
+            $this->bot->editMessageReplyMarkup([
+                'reply_markup' => $this->bot->settingMarkup(),
+            ]);
+        } else {
+            $this->bot->answerCallbackQuery('Something went wrong!');
+        }
     }
 }
